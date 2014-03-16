5c052e1844819dd4bd93f9b252c022b73bfed842
José Conti <j.conti@joseconti.com>
2014-03-16 15:15:45 +0100

    Updating Uninstall file

--- uninstall.php
+++ uninstall.php
@@ -12,28 +12,30 @@ delete_option("wpvarnish_timeout");
 delete_option("wpvarnish_use_adminport");
 delete_option("wpvarnish_use_version");
 delete_option("wpvarnish_server");
+/* PLUGIN VERSION */
+delete_site_option("wpvarnish_version");
 /* SERVER 1 */
-delete_option("wpvarnish_addr_1");
-delete_option("wpvarnish_port_1");
-delete_option("wpvarnish_secret_1");
-delete_option("wpvarnish_timeout_1");
-delete_option("wpvarnish_use_adminport_1");
-delete_option("wpvarnish_use_version_1");
-delete_option("wpvarnish_server_1");
+delete_site_option("wpvarnish_addr_1");
+delete_site_option("wpvarnish_port_1");
+delete_site_option("wpvarnish_secret_1");
+delete_site_option("wpvarnish_timeout_1");
+delete_site_option("wpvarnish_use_adminport_1");
+delete_site_option("wpvarnish_use_version_1");
+delete_site_option("wpvarnish_server_1");
 /* SERVER 2 */
-delete_option("wpvarnish_addr_2");
-delete_option("wpvarnish_port_2");
-delete_option("wpvarnish_secret_2");
-delete_option("wpvarnish_timeout_2");
-delete_option("wpvarnish_use_adminport_2");
-delete_option("wpvarnish_use_version_2");
-delete_option("wpvarnish_server_2");
+delete_site_option("wpvarnish_addr_2");
+delete_site_option("wpvarnish_port_2");
+delete_site_option("wpvarnish_secret_2");
+delete_site_option("wpvarnish_timeout_2");
+delete_site_option("wpvarnish_use_adminport_2");
+delete_site_option("wpvarnish_use_version_2");
+delete_site_option("wpvarnish_server_2");
 /* SERVER 3 */
-delete_option("wpvarnish_addr_3");
-delete_option("wpvarnish_port_3");
-delete_option("wpvarnish_secret_3");
-delete_option("wpvarnish_timeout_3");
-delete_option("wpvarnish_use_adminport_3");
-delete_option("wpvarnish_use_version_3");
-delete_option("wpvarnish_server_3");
+delete_site_option("wpvarnish_addr_3");
+delete_site_option("wpvarnish_port_3");
+delete_site_option("wpvarnish_secret_3");
+delete_site_option("wpvarnish_timeout_3");
+delete_site_option("wpvarnish_use_adminport_3");
+delete_site_option("wpvarnish_use_version_3");
+delete_site_option("wpvarnish_server_3");
 ?>
